530dbf52ddb35d358c8d52403fcc13fb7bac3c87
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2025-05-13 13:20:16 -0300

    Update init.sql

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "fiap-cliente-service";
+CREATE DATABASE "fiapestoque";
+CREATE DATABASE "pagamento-service";
+CREATE DATABASE "fiap-produto-service";
+CREATE DATABASE "fiap-pedido-service";
